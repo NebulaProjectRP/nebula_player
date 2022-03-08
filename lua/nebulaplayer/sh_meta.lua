c71ba569386e8e301f44d4dd567d1b63df397f35
Gonzalolog <gonzalolog@gmail.com>
2022-03-08 19:59:42 -0300

    Added basic vip groups

--- lua/nebulaplayer/sh_meta.lua
+++ lua/nebulaplayer/sh_meta.lua
@@ -1,1 +1,12 @@
-local a
+local meta = FindMetaTable("Player")
+
+local vipgroups = {
+    ["admin"] = true,
+    ["superadmin"] = true,
+    ["owner"] = true,
+    ["vip"] = true,
+    ["vip+"] = true,
+}
+function meta:isVip()
+    return vipgroups[self:GetUserGroup()] != nil
+end
