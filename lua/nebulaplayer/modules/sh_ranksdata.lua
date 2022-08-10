ce49dec6143238058bf2686e3144aa14de58d51f
Gonzalolog <gonzalolog@gmail.com>
2022-08-10 04:13:46 -0300

    Added rank getter func

--- lua/nebulaplayer/modules/sh_ranksdata.lua
+++ lua/nebulaplayer/modules/sh_ranksdata.lua
@@ -13,6 +13,7 @@ NebulaRanks.Ranks["default"] = {
     Name = "",
     Props = 45,
     Perm = RANK_LOWEST,
+    MaxStore = 5,
     Hidden = true,
     Color = Color( 133, 133, 133),
 }
@@ -20,6 +21,7 @@ NebulaRanks.Ranks["default"] = {
 NebulaRanks.Ranks["cosmic"] = {
     Name = "Cosmic Born",
     Props = 60,
+    MaxStore = 8,
     Perm = RANK_MEDIUM,
     Color = Color( 221, 52, 255),
 }
@@ -37,3 +39,7 @@ NebulaRanks.Ranks["kat"] = {
     Perm = RANK_HIGHEST,
     Color = Color( 252, 173, 255),
 }
+
+function NebulaRanks:GetField(ply, field)
+    return (self.Ranks[rank] and self.Ranks[rank] or self.Ranks["default"])[field]
+end
