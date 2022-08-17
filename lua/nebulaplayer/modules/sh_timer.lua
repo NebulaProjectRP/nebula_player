2ea6ef10818846f3e981fdfaf36af2d7c46302e2
Gonzalolog <gonzalolog@gmail.com>
2022-08-17 12:37:52 -0300

    Update sh_timer.lua

--- lua/nebulaplayer/modules/sh_timer.lua
+++ lua/nebulaplayer/modules/sh_timer.lua
@@ -9,7 +9,7 @@ local meta = FindMetaTable("Player")
 
 function meta:getPlayTime()
     if CLIENT then
-        return CurTime() - NebulaPlayer.PlayTime.session + NebulaPlayer.PlayTime.time
+        return math.Round(CurTime() - NebulaPlayer.PlayTime.session + NebulaPlayer.PlayTime.time)
     else
         return CurTime() - self.startTime + self.playTime.time
     end
@@ -28,4 +28,23 @@ if CLIENT then
             session = CurTime()
         }
     end)
+
+    function string.NicePlayTime()
+        local time = LocalPlayer():getPlayTime()
+        local format = os.date("!*t", time)
+        local str = ""
+        if (format.yday > 0) then
+            str = str .. string.format("%d day%s, ", format.yday, format.yday > 1 and "s" or "")
+        end
+
+        if (format.hour > 0) then
+            str = str .. string.format("%d hour%s, ", format.hour, format.hour > 1 and "s" or "")
+        end
+
+        if (format.min > 0) then
+            str = str .. string.format("%d minute%s", format.min, format.min > 1 and "s" or "")
+        end
+
+        return str
+    end
 end
