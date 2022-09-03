f382f7757c76a733f3d1d1ba669bbc3b9383b118
Gonzalolog <gonzalolog@gmail.com>
2022-09-03 19:15:32 -0300

    Removed old inarena method from ply lib

--- lua/nebulaplayer/sh_meta.lua
+++ lua/nebulaplayer/sh_meta.lua
@@ -11,22 +11,9 @@ function meta:isVip()
     return vipgroups[self:GetUserGroup()] != nil
 end
 
-function meta:InArena()
-    return self:GetNWBool("InArena", false)
-end
-
 hook.Add("OnPortalEntered", "NebulaArenaChecker", function(ply, ent, exit)
-    if (ent:GetGroup() == "arena") then
-        ply:SetNWBool("InArena", true)
-    end
-
-    if (ent:GetGroup() == "exit_arena") then
-        ply:SetNWBool("InArena", false)
-    end
 end)
 
 hook.Add("PlayerDeath", "NebulaArenaChecker", function(ply)
-    if (ply:InArena()) then
-        ply:SetNWBool("InArena", false)
-    end
+
 end)
